011812ec2da1e85117f4b8f6680fdad923cb0837
Surya sharma <suryasharma73077@gmail.com>
2025-08-11 09:49:39 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	int x;
+//	cout<<" Enter Number : ";
+//	cin>>x;
+//	int y = x;
+//	int count = 0;
+//	int sum = 0;
+//	int product =1;
+//	int remender=0;
+//	int even_sum = 0;
+//	int reverse=0;
+//	int reverse_sum = 0;
+//	do{
+//	 remender = x%10;
+//	 if(x>=0){
+//	 	reverse = reverse*10+remender;
+//	 	reverse_sum = reverse_sum + remender;
+//	 }
+//	 x = x/10;
+//	 count ++;
+//	 sum = sum + remender;
+//	 if(remender == 0) continue;
+//	 else product = product * remender;
+//	 if(remender%2==0) even_sum = even_sum + remender;
+//    }while(x>0);
+//    cout<<" The Input Number is : "<<y<<endl;
+//	cout<<" The Length of a Number is : "<<count<<endl;
+//	cout<<" The Sum of a Number is : "<<sum<<endl;
+//	cout<<" The Reverse of a Number is : "<<reverse<<endl;
+//	cout<<" The Sum of a Reverse of a Number is : "<<reverse_sum<<endl;
+//	cout<<" The Sum of Even Number is : "<<even_sum<<endl;
+//	cout<<" The Product of a Number is : "<<product<<" Except '0' "<<endl;
+	
+//  //_______________________factorial of number___________________
+//
+//    int fact;
+//    int n;
+//    cout<<" Enter number : ";
+//    cin>>n;
+//    for(int j= 1 ;j<=n;j++){
+//    	fact=1;
+//    	cout<<j<<"! => ";
+//    	for(int i=1;i<=j;i++){
+//    	    fact = fact*i;
+//    	if(i<=j){
+//    		cout<<i<<" ";
+//    	if(i<j){
+//    		cout<<"x ";
+//		}
+//		}
+//	}
+//	cout<<"= ";
+//	cout<<fact<<" "<<endl;
+//	}
+
+//  //_______________print ASCII value____________________
+
+//    int x=65;
+//    while(x<=90){
+//    	cout<<x<<" = "<<char(x)<<endl;
+//    	x++;
+//	}
+}
