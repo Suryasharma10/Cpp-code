a0b6b8c6ec2c7d4d2e4413e94841de4f5bed1ab8
Surya sharma <suryasharma73077@gmail.com>
2025-08-11 09:48:01 +0530

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	cout<<"Hello\n";
+//	cout<<"I am begginer ";
+//	cout<<endl;
+//	cout<<", Learning C++"<<endl<<"wanted to deep dive in it"<<endl;
+//	cout<<78;
+//____________________comment_using__'//'___________________
+//	//________________________variables________________________
+//	int x,y;
+//	float z=11.6;
+//	cout<<"\n";
+//	cout<<z<<endl;
+//	cout<<"Enter Value of x : ";
+//	cin>>x;
+//	cout<<"Enter Value of y : ";
+//	cin>>y;
+//	cout<<"value of x + y is : "<<x+y<<endl;
+//	cout<<"value of x % y is : "<<x%y<<endl;// When Both the Variables are of Same Data Type
+//	cout<<"value of x * y is : "<<x*y<<endl;
+//	x= x/4;
+//	y= y*7;
+//	cout<<x<<endl;
+//	cout<<y++<<endl;
+//	cout<<y<<endl;
+//	
+//	// ________________________Area of a Circle________________________
+//	// a=pie*r*r;
+//	double area;
+//	float pie = 3.141592;
+//	float radius;
+//	cout<<"Enter Radius : ";
+//	cin>>radius;
+//	area = pie*radius*radius;
+//	cout<<"Area of a Circle is : ";
+//	cout<<area<<endl;
+//	
+//	// _________________________Volume Of a Sphere ____________________
+//	// s = (4/3)*pie*r*r*r;
+//	
+//	float Sphere = (4/3)*pie*radius*radius*radius;
+//	cout<<"Volume of a Sphere is : ";
+//	cout<<Sphere<<endl;
+//	
+//	// _______________________Calculating Simple Interest_____________
+//	//SI=(p*r*t)/100
+//	
+//	float Simple_Interest ;
+//	cout<<"Calculating Simple Interest ....!"<<endl;
+//	float Principle;
+//	cout<<"Enter Principle : ";
+//	cin>>Principle;
+//	float Rate;
+//	cout<<"Enter Rate : ";
+//	cin>>Rate;
+//	float Amount;
+//	cout<<"Enter Amount : ";
+//	cin>>Amount;
+//	Simple_Interest = (Principle*Rate*Amount)/100;
+//	cout<<"Your Simple Interest is : ";
+//	cout<<Simple_Interest<<endl;
+//	
+//    // __________________________Type Casting_________________________
+//	//Convert one Data Type to Another Data Type.
+//	int num ;
+//	cout<<"Enter number"<<endl;
+//	cin>>num;
+//	float y = (float)num;
+//	cout<<y/2;
+//	
+//	//______________hierarchy of operators______________
+	float i = (float)7/22*(3.14+2)*3/5;
+	float n = 4+2%-8;
+	cout<<i<<endl<<n<<endl;
+	int l = 106;
+	cout<<"106 = "<<(char)l<<endl;
+	char ch1 = '0',ch2='V';
+	int x = (int)ch1;
+	int y = (int)ch2;
+	cout<<"0 = "<<x<<endl<<"V = "<<y<<endl;
+	
+//	//____________________ASCII Value___________________
+//	//A-Z =(65-90); 
+//	//a-z =(97-122); 
+//	//0-9 =(48-57);
+//	float d = 2/7;
+//	cout<<d;
+}
