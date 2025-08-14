fab60c84f1ee03239b576d8fb8352a979572c5d4
Surya Sharma <suryasharma73077@gmail.com>
2025-08-14 16:42:40 +0530

    Add files via upload

---
+++ 2DArray.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	int a[2][3]={{1,2,3},{4,5,6}};
+//	int b[][3]={11,3,24,4,5,60,17,8,83};
+//	int arr[2][3];
+//	//	arr[2][0]=13;
+//	//	arr[2][2]=1;
+//	//	arr[3][1]=65;
+//	for(int i=0;i<2;i++){
+//		for(int j=0;j<3;j++){
+//			cout<<b[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//	for(int i=0;i<2;i++){
+//		for(int j=0;j<3;j++){
+//			cout<<a[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//	cout<<"Enter Array Numbers : ";
+//	for(int i=0;i<2;i++){
+//		for(int j=0;j<3;j++){
+//			cin>>arr[i][j];
+//		}
+//		cout<<endl;
+//	}
+//	cout<<"Arrays Row wise : "<<endl;
+//	for(int i=0;i<2;i++){
+//		for(int j=0;j<3;j++){
+//			cout<<arr[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//	//_________transpose of a matrix_____________
+//	cout<<"Arrays column wise : "<<endl;
+//	for(int i=0;i<3;i++){
+//		for(int j=0;j<2;j++){
+//			cout<<arr[j][i]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//    int c[5][5];
+//	cout<<"2DArrays Stores 10 : "<<endl;
+//	for(int i=0;i<5;i++){
+//		for(int j=0;j<5;j++){
+//			c[i][j]=10;
+//			cout<<c[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//	int d[5][5];
+//	cout<<"2DArrays Stores 9 : "<<endl;
+//	for(int i=0;i<5;i++){
+//		for(int j=0;j<5;j++){
+//			d[i][j]=9;
+//			cout<<d[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//	cout<<"2DArrays Sum : "<<endl;
+//	for(int i=0;i<5;i++){
+//		for(int j=0;j<5;j++){
+//			cout<<c[i][j] + d[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//    int e[3][3]={{34,53,12},{7,86,3},{33,8,90}};
+//    int temp =e[0][0];
+//	cout<<"Elements in a 2DArray : "<<endl;
+//	for(int i=0;i<3;i++){
+//		for(int j=0;j<3;j++){
+//			if(e[i][j]>temp){
+//			temp =e[i][j];	
+//			}
+//			cout<<e[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//	cout<<endl<<"Max Element : "<<temp<<endl;
+//	for(int i=0;i<3;i++){
+//		for(int j=0;j<3;j++){
+//			if(temp>e[i][j]){
+//			temp =e[i][j];	
+//			}
+//		}
+//	}
+//	cout<<"Min Element : "<<temp<<endl;   
+//	int sum=0;
+//	for(int i=0;i<3;i++){
+//		for(int j=0;j<3;j++){
+//		sum+=e[i][j];
+//		}
+//	}
+//	cout<<"Sum of Elements : "<<sum<<endl;  
+//	long long product=1;
+//	for(int i=0;i<3;i++){
+//		for(int j=0;j<3;j++){
+//			product*=e[i][j];
+//		}
+//	}
+//	cout<<"Product of Elements : "<<product<<endl;  
+//  //__________________sum in range _______________________
+//    int rec[5][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
+//    int sum=0;
+//    // ___________cordinates (2,1),(4,3)_____________
+//    for(int i=2;i<=4;i++){
+//		for(int j=1;j<=3;j++){
+//			sum+=rec[i][j];
+//		}
+//	}
+//	cout<<"Sum : (2,1) to (4,3) : "<<sum<<endl;		
+}
