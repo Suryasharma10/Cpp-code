f0847b4a14e1ec1d5d66e16174395a1223d8e637
Surya Sharma <suryasharma73077@gmail.com>
2025-08-14 08:06:51 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	int arr[5];
+//	cout<<"Enter Array : ";
+//	for(int i=0;i<5;i++){
+//		cin>>arr[i];
+//	}
+//	for(int i=0;i<5;i++){
+//	    cout<<arr[i]<<" ";	
+//	}
+//	arr[0]=76;
+//	cout<<endl;
+//	for(int i=0;i<5;i++){
+//	    cout<<arr[i]<<" ";	
+//	}
+//__________________________________________________
+//    int y[3]= {1,2,3};
+//    int x[]={2,12,5,7,1,3,11,15};
+//    int n = sizeof(x)/4;
+//	for(int i=0;i<n;i++){
+//		cout<<x[i]*2<<" ";
+//	}	
+//	cout<<endl;
+//	int sum = 0;
+//	int product = 1;
+//	for(int i=n-1;i>=0;i--){
+//		sum+=x[i];
+//		product*=x[i];
+//		cout<<x[i]*2<<" ";
+//	}
+//	cout<<endl<<"sum : "<<sum<<endl;
+//	cout<<"product : "<<product<<endl;
+//	//int b[size];//error
+//	int size=10;
+//	int b[size];//no error
+//	//________Array is pass by reference in function__________
+//	// _______because function takes address of array_________
+//    int n;
+//    cout<<"Enter size of Array : ";
+//    cin>>n;
+//    cout<<"Enter Array : ";
+//    int arr[n];
+//    for(int i=0;i<n;i++){
+//    	cin>>arr[i];
+//	}
+//	int max_=0;
+//	for(int i=0;i<n;i++){
+//	//		if(max_>arr[i]);
+//	//		else{
+//	//			max_=arr[i];
+//	//		}
+//		max_ = max(max_,arr[i]);
+//	}
+//	cout<<"Maximum number is : "<<max_<<endl;
+//	int min_=arr[0];
+//	for(int i=0;i<n;i++){
+//	//		if(min_<arr[i]);
+//	//		else{
+//	//			min_=arr[i];
+//	//		}
+//	    min_ = min(min_,arr[i]);
+//	}
+//	cout<<"Minimum number is : "<<min_<<endl;
+	cout<<INT_MIN<<endl;
+	cout<<INT_MAX<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
