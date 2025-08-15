75b3257812865313ba4b5ae6f7e3986f3769eb44
Surya Sharma <suryasharma73077@gmail.com>
2025-08-15 22:18:34 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+//	char ch[] = {'a','p','p','l','e'};
+//	cout<<ch[0]<<ch[1]<<ch[2]<<ch[3]<<ch[4]<<endl;
+//	string x ="Raghav";//string is a set of character or character Array
+//	cout<<x<<endl;
+//	cout<<x[2]<<endl;
+//	x[2]='m';
+//	cout<<x<<endl;
+//	string name ;
+//	//cin>>name;//takes only first world before space 
+//	getline(cin,name);//takes whole line 
+//	cout<<name<<endl;
+//  //______________________Built in function__________________
+//    string s ="Ramesh Pal is a Good Boy";
+//    cout<<s<<endl;
+//    cout<<s.length()<<endl;
+//    cout<<s.size()<<endl;
+//    int count = 0;
+//    for(int i=0;i<s.length();i++){
+//    	if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+//    		count++;
+//		}
+//	}
+//	cout<<"Vowels : "<<count<<endl;
+//    for(int i=0;i<s.length();i++){
+//    	if(i%2==0){
+//    		s[i]='#';
+//		}else;
+//		cout<<s[i];
+//	}
+//    s.push_back('s');
+//    s.push_back(' ');
+//    s.push_back('h');
+//    s.push_back('i');
+//	cout<<s<<endl;
+//	string nw="Good";
+//	//	nw.erase(nw.size()-2);
+//	//	cout<<nw<<endl;
+//	nw.append(" Morning");
+//	cout<<nw<<" "<<nw.length()<<endl;
+//	//	nw.clear();//empty string
+//	//	cout<<nw<<endl;
+//	//___________plus Operator______________
+//	nw = "Surya ! " + nw ;
+//	cout<<nw<<endl;
+//	//____________reverse__________________
+//	string s = "Happy";
+//	reverse(s.begin(),s.end());
+//	cout<<s<<endl;
+//  //_____________reverse up to the mid element___
+//    int n;
+//    string even ;
+//    int len;
+//    cout<<"Enter Even Number For String : ";
+//    cin>>n;
+//    if(n%2==0){
+//    	cout<<"Enter String : ";
+//    	cin>>even;
+//    	len = even.length();
+//    	if(len%2!=0){
+//    		cout<<"Length of a string is not even "<<endl;
+//    		exit;
+//		}
+//	}else{
+//		cout<<"You haven't Enter Even Number : "<<endl;
+//		exit;
+//	}
+//	//	int i=0;
+//	//	int j=len/2-1;
+//	//	while(i<j){
+//	//		char temp = even[i];
+//	//		even[i]=even[j];
+//	//		even[j]=temp;
+//	//		i++;
+//	//		j--;
+//	//	}
+//	//	reverse(even.begin()+2,even.begin()+5);//index 1 to index 4
+//	//    cout<<even<<endl;
+//	reverse(even.begin(),even.begin()+n/2);
+//    cout<<even<<endl;	
+//     // _________to string_____________________
+//     int x =2355;
+//     string a = to_string(x);
+//     //a = a + "good";
+//    cout<<a.length()<<endl;
+//     cout<<a<<endl;
+//     //__________to int____________________
+       string b = "1234";   
+       int c = stoi(b); // string to int
+       c
+       cout<<c<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
