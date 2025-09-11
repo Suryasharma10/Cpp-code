58f6e41fb2dfaec08821a03087e6fa97168e86ee
Surya Sharma <suryasharma73077@gmail.com>
2025-09-11 14:30:02 +0530

    Add files via upload

---
+++ ArrayRevision.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	int n;
+//	int sum=0;
+//	cout<<"Enter number of Elements to Store in Array : ";
+//	cin>>n;
+//	int arr[100];
+//	int temp;
+//	cout<<"Enter Array numbers : "<<endl;
+//	for(int i=1;i<=n;i++){
+//		cin>>arr[i];
+//	}
+//	int max=0;
+//	int min=arr[0];
+//	cout<<"Your values in Reverse Order are : ";
+//	for(int i=n;i>=1;i--){
+//		cout<<arr[i]<<" ";
+//	}
+//    for(int i=1;i<=n;i++){
+//    	sum=sum+arr[i];
+//    	temp = arr[i];
+//    	arr[i]=arr[n-i+1];
+//    	cout<<arr[i]<<" ";
+//    	arr[i]=temp;
+//	}
+//	cout<<endl<<"Sum of all Elements are : "<<sum<<endl;
+	
+//	for(int i=1;i<=n;i++){
+//		if(max<arr[i]){
+//    		max=arr[i];
+//		}
+//		if(min>arr[i]){
+//			min=arr[i];
+//		}
+//	}
+//	
+//	cout<<"Maximum Element is : "<<max<<endl;
+//	cout<<"Minmum Element is : "<<min<<endl;
+//------------------------------------------------------------------------------------------------------
+//  //----------------count fequency of an Array-----------
+//	int arr[] = {2,3,4,2,2,4,4,6,7,2,4,6,5,1,8,7,3,2,2,22,24,32,27623};
+//	int target = 2;
+//	int n = sizeof(arr)/4;
+//	int arr[100];
+//	int n;
+//	cout<<"Enter number of elements in Array :  ";
+//	cin>>n;
+//	for(int i=0;i<n;i++){
+//		cin>>arr[i];
+//	}
+//	cout<<"Your Array is : "<<endl;
+//	for(int i=0;i<n;i++){
+//		cout<<arr[i]<<" , ";
+//	}
+//	cout<<endl;
+//	for(int i=0;i<n;i++){
+//		if(arr[i]<=arr[i+1]){
+//			if(arr[i]==arr[n-1] && arr[i]<arr[i+1]){
+//				cout<<"Array is Sorted : "<<endl;
+//			}
+//		}else{
+//			cout<<"Not a sorted Array : "<<endl;
+//			break;
+//		}
+//	}
+//	int target;
+//	cout<<endl<<"Enter Target Element : ";
+//	cin>>target;
+//	int count=0;
+//	int even=0;
+//	for(int i=0;i<n;i++){
+//		if(target ==arr[i]){
+//			count++;
+//		}
+//		if(arr[i]%2==0){
+//			even++;
+//		}
+//	}
+//    cout<<"The number "<<target<<" Frequency is "<<count<<endl;
+//    cout<<"Total Even Number Are : "<<even<<endl;
+//    	
+}
