3b89d05baee2bd7d2ea7f9dd0c00751a738e2010
Surya Sharma <suryasharma73077@gmail.com>
2025-10-07 13:55:54 +0530

    Add files via upload

--- vector.cpp
+++ vector.cpp
@@ -12,26 +12,35 @@ int main(){
 		// for(char val : name){
 		// 	cout<<val<<endl;
 		// }
-	vector <int> num ;
-	cout<<"before Size = "<<num.size()<<endl;
-	num.push_back(51);
-	num.push_back(32);
-	num.push_back(1);
-	num.push_back(92);
-	num.push_back(2);
-	cout<<"after push back Size = "<<num.size()<<endl;
-	for(int val : num){
-		cout<<val<<" ";
+	// vector <int> num ;
+	// cout<<"before Size = "<<num.size()<<endl;
+	// num.push_back(51);
+	// num.push_back(32);
+	// num.push_back(1);
+	// num.push_back(92);
+	// num.push_back(2);
+	// cout<<"after push back Size = "<<num.size()<<endl;
+	// for(int val : num){
+	// 	cout<<val<<" ";
+	// }
+	// cout<<endl;
+	// num.pop_back();
+	// cout<<"after pop back Size = "<<num.size()<<endl;
+	// for(int val : num){
+	// 	cout<<val<<" ";
+	// }
+	// cout<<"\nFront value = "<<num.front();
+	// cout<<"\nBack value = "<<num.back();
+	// cout<<"\nvalue at 1 index = "<<num.at(1)<<endl; 
+	// cout<<num.capacity()<<endl;
+	// vector<int> v={1,2,1,3,2,4,3,5,4,6,5,7};
+	// v.clear();
+	// cout<<"Size = "<<v.size();
+	vector<int> v1={1,2,3,4,5,2,3,4,1};
+	int ans;
+	for(int i=0;i<v1.size();i++){
+		ans = ans ^ v1[i];
 	}
-	cout<<endl;
-	num.pop_back();
-	cout<<"after pop back Size = "<<num.size()<<endl;
-	for(int val : num){
-		cout<<val<<" ";
-	}
-	cout<<"\nFront value = "<<num.front();
-	cout<<"\nBack value = "<<num.back();
-	cout<<"\nvalue at 1 index = "<<num.at(1)<<endl; 
-	cout<<num.capacity();
+	cout<<ans;
 	return 0;
 }
